9b7e0564c257fd9243bcbefd6476593853ebd8e0
Camillo Facello <hello@camillof.dev>
2024-02-28 10:06:46 -0300

    Improve text to publish and use periodicity

--- main.rb
+++ main.rb
@@ -26,7 +26,7 @@ def prepare
   @discarded_news = []
 
   if ENV['NEWS_RSS_SOURCES'].nil? || ENV['MOOD_THRESHOLD'].nil? || ENV['PERIODICITY'].nil?
-    puts 'Please set the NEWS_RSS_SOURCES and MOOD_THRESHOLD and PERIODICITY environment variables.'
+    puts 'Please set the NEWS_RSS_SOURCES, MOOD_THRESHOLD and PERIODICITY environment variables.'
     exit
   end
 
@@ -52,12 +52,15 @@ def process_news
   @news_rss_sources.each do |url|
     URI.open(url) do |rss|
       feed = RSS::Parser.parse(rss, validate: false)
-      feed.items.each do |item|
+      feed.items.filter { |i| i.pubDate.present? }.each do |item|
         mood = tm.analyze(item.title)
 
         item = NewsItem.new(item.title, item.category, item.link, item.pubDate, mood)
 
-        if mood < @mood_threshold || @excluded_categories.include?(item.category)
+        if mood < @mood_threshold ||
+          @excluded_categories.include?(item.category) ||
+          item.pub_date < @periodicity.minutes.ago
+
           @discarded_news << item
         else
           @filtered_news << item
@@ -69,15 +72,22 @@ end
 
 def publish_news
   puts "Item to publish: #{@filtered_news.length}"
+
   @filtered_news.sort { |a, b| a.pub_date <=> b.pub_date }.each do |item|
-    post_tweet(item.title)
+    puts "Publishing: #{item.title}" if ENV['DEBUG'] == 'true'
+    text = <<~TEXT
+      #{item.title}
+      Fuente: #{item.link}
+    TEXT
+
+    publish_item(text) if ENV['PUBLISH_NEWS'] == 'true'
   end
 end
 
 def run
   prepare
   process_news
-  publish_news if ENV['PUBLISH_NEWS'] == 'true'
+  publish_news
 end
 
 run
